67b064e53939d5161a33708438feeab94c46baae
Brad Chen <brad5620732@hotmail.com>
2012-11-24 08:21:09 -0500

    Add the ability to delete user.

--- sample/includes/data_list.php
+++ sample/includes/data_list.php
@@ -1,10 +1,42 @@
+<?php
+/**
+ * Display the data and provide delete function.
+ *
+ * @author Brad Chen
+ */
+
+if (isset($_GET['delete'])) {
+  $id = intval($_GET['delete']);
+  if (isset($_SESSION['users'][$id])) {
+    unset($_SESSION['users'][$id]);
+    $_SESSION['MESSAGE'] = '<strong>Success!</strong> '
+      . 'The user has been deleted.';
+  } else {
+    $_SESSION['ERROR'] = '<strong>Error!</strong> '
+      . 'The user could not be found.';
+  }
+  header('Location: ' . $_SERVER['PHP_SELF']);
+  exit(0);
+}
+?>
+<script type="text/javascript">
+// <![CDATA[
+jQuery(function() {
+  jQuery('button.remove').click(function() {
+    var id = jQuery(this).data('id');
+    window.location = '<?php echo $_SERVER['PHP_SELF']; ?>?delete=' + id;
+  });
+});
+// ]]>
+</script>
 <h2>List of Users</h2>
 <table class="table table-striped">
   <thead>
     <tr>
-      <th>#</th>
-      <th>Login</th>
-      <th>Password</th>
+      <th style="width: 8%;">#</th>
+      <th style="width: 40%;">Login</th>
+      <th style="width: 40%;">Password</th>
+      <td></th>
     </tr>
   </thead>
   <tbody>
@@ -13,6 +45,10 @@
         <td><?php echo $i + 1; ?></td>
         <td><?php echo $user['login']; ?></td>
         <td><?php echo $user['password']; ?></td>
+        <td>
+          <button class="btn remove" data-id="<?php echo $i; ?>"><i
+            class="icon-remove"></i> Delete</button>
+        </td>
       </tr>
     <?php endforeach; ?>
   </tbody>
